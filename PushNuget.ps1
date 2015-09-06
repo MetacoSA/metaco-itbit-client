ecf44ceb86ca3d2c66436906f3c0564865768290
lontivero <lucasontivero@gmail.com>
2015-09-06 20:31:56 -0300

    Version Change for Nuget package

--- PushNuget.ps1
+++ PushNuget.ps1
@@ -3,10 +3,10 @@ param (
 )
 
 del *.nupkg
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ".\ItBitClient\ItBitClient.csproj" -p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ".\MetacoItBitClient\MetacoItBit.csproj" -p:Configuration=Release
 
 
-.\.nuget\NuGet.exe pack .\ItBitClient\ItBitClient.csproj -Prop Configuration=Release
+.\.nuget\NuGet.exe pack .\MetacoItBitClient\MetacoItBit.csproj -Prop Configuration=Release
 
 forfiles /m *.nupkg /c "cmd /c .\.nuget\NuGet.exe push @FILE FEipouFE987F2BBO3UD8387A64Hhh -source https://nuget.org/ -ApiKey $apikey"  
 (((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
